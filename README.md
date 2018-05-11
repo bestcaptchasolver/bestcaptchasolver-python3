dd14f8677f981bfb9285bb59501afe1b6baffb7a
bestcaptchasolver <bcsolver@gmail.com>
2018-05-11 15:37:10 +0000

    1st commit

---
+++ README.md
@@ -0,0 +1,88 @@
+BestCaptchaSolver.com python3 API wrapper
+=========================================
+
+bestcaptchasolver-python3 is a super easy to use bypass captcha python3 API wrapper for bestcaptchasolver.com captcha service
+
+## Installation    
+    git clone https://github.com/bestcaptchasolver/bestcaptchasolver-python3
+
+## Dependencies
+    pip install requests
+
+## Usage
+    # make sure you've changed access_key, page_url, etc in main.py
+    python3 main.py  
+
+## How to use?
+
+Simply require the module, set the auth details and start using the captcha service:
+
+``` python
+from bestcaptchasolverapi3.bestcaptchasolverapi import BestCaptchaSolverAPI
+```
+Set access_token for authentication
+
+``` python
+access_token = 'access_token_here'
+# get your access token from https://bestcaptchasolver.com/account
+bcs = BestCaptchaSolverAPI(ACCESS_TOKEN)
+```
+
+Once you've set your authentication details, you can start using the API
+
+**Get balance**
+
+``` python
+balance = bcs.account_balance()                 
+```
+
+**Submit image captcha**
+
+``` python
+bcs.submit_image_captcha('captcha.jpg', False)    # submit image captcha (case_sensitive param optional)
+```
+The image submission works with both files and b64 encoded strings.
+Also for case-sensitive captchas, the 2nd parameter set to True, will make sure case is taken into account.
+
+**Submit recaptcha details**
+
+For recaptcha submission there are two things that are required.
+- page_url
+- site_key
+- proxy (optional)
+``` python
+bcs.submit_recaptcha(PAGE_URL, SITE_KEY)   
+```
+This method returns a captchaID. This ID will be used next, to retrieve the g-response, once workers have 
+completed the captcha. This takes somewhere between 10-80 seconds.
+
+**Retrieve captcha response (both image and recaptcha)**
+
+```
+image_text = bcs.retrieve(captcha_id)
+gresponse = bcs.retrieve(recaptcha_id)
+```
+
+**Set captcha bad**
+
+When a captcha was solved wrong by our workers, you can notify the server with it's ID,
+so we know something went wrong.
+
+``` python
+bcs.set_captcha_bad(captcha_id)
+```
+
+## Examples
+Check main.py
+
+## License
+API library is licensed under the MIT License
+
+## More information
+More details about the server-side API can be found [here](https://bestcaptchasolver.com/api )
+
+
+<sup><sub>captcha, bypasscaptcha, decaptcher, decaptcha, 2captcha, deathbycaptcha, anticaptcha, 
+bypassrecaptchav2, bypassnocaptcharecaptcha, bypassinvisiblerecaptcha, captchaservicesforrecaptchav2, 
+recaptchav2captchasolver, googlerecaptchasolver, recaptchasolverpython, recaptchabypassscript, bestcaptchasolver</sup></sub>
+
---
+++ bestcaptchasolverapi3/bestcaptchasolverapi.py
@@ -0,0 +1,106 @@
+try:
+    from requests import session
+    import requests
+except:
+    raise Exception('requests package not installed, try with: \'pip2.7 install requests\'')
+
+import os, json
+from base64 import b64encode
+
+USER_AGENT = 'pythonClient'
+BASE_URL = 'https://bcsapi.xyz/api'
+SSL_VERIFY = False
+# endpoints
+# -------------------------------------------------------------------------------------------
+
+
+# API class
+# -----------------------------------------
+class BestCaptchaSolverAPI:
+    def __init__(self, access_token, timeout = 120):
+        self._access_token = access_token
+        self._data = {
+            'access_token': access_token
+        }
+
+        self._timeout = timeout
+        self._session = session()       # init a new session
+
+        self._headers = {               # use this user agent
+            'User-Agent' : USER_AGENT
+        }
+
+    # get account balance
+    def account_balance(self):
+        url = '{}/user/balance?access_token={}'.format(BASE_URL, self._access_token)
+        resp = self.GET(url)
+        return '${}'.format(resp['balance'])
+
+    # solve normal captcha
+    def submit_image_captcha(self, image_path, case_sensitive = False):
+        data = dict(self._data)
+        url = '{}/captcha/image'.format(BASE_URL)
+        if os.path.exists(image_path):
+            with open(image_path, 'rb') as f:
+                data['b64image'] = b64encode(f.read())
+        else:
+            data['b64image'] = image_path       # should be b64 already
+
+        if case_sensitive: data['case_sensitive'] = '1'
+
+        resp = self.POST(url, data)
+        return resp['id']       # return ID
+
+    # submit recaptcha to system
+    # SET PROXY AS WELL
+    # -------------------
+    # ----------------------------------
+    # ------------------------------
+    def submit_recaptcha(self, page_url, site_key, proxy = None):
+        # data parameters
+        data = dict(self._data)
+        data['page_url'] = page_url
+        data['site_key'] = site_key
+
+        # check proxy and set dict (request params) accordingly
+        if proxy:   # if proxy is given, check proxytype
+            # we have both proxy and type at this point
+            data['proxy'] = proxy
+            data['proxytype'] = 'HTTP'
+
+        # make request with all data
+        url = '{}/captcha/recaptcha'.format(BASE_URL)
+        resp = self.POST(url, data)
+        return resp['id']  # return ID
+
+    # retrieve captcha
+    def retrieve(self, captcha_id = None):
+        url = '{}/captcha/{}?access_token={}'.format(BASE_URL, captcha_id, self._access_token)
+        resp = self.GET(url)
+        try:
+            if resp['status'] == 'pending': return None
+        except:
+            pass
+        try:
+            return resp['gresponse']
+        except:
+            return resp['text']
+
+    # set captcha bad, if given id, otherwise set the last one
+    def set_captcha_bad(self, captcha_id):
+        data = dict(self._data)
+        url = '{}/captcha/bad/{}'.format(BASE_URL, captcha_id)
+        resp = self.POST(url, data)
+        return resp['status']
+
+    def GET(self, url):
+        r = self._session.get(url, headers=self._headers, timeout=self._timeout, verify=SSL_VERIFY)
+        js = json.loads(r.text)
+        if js['status'] == 'error': raise Exception(js['message'])
+        return js
+
+    def POST(self, url, data):
+        r = self._session.post(url, data=data, headers=self._headers, timeout=self._timeout, verify=SSL_VERIFY)
+        js = json.loads(r.text)
+        if js['status'] == 'error': raise Exception(js['message'])
+        return js
